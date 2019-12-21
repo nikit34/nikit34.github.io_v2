3b8d0fb15e4816a06b1b2bc070a7b2c3c4bb3fd6
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2019-12-21 13:25:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -37,7 +37,6 @@
 $(window).on("scroll touchmove", function () {
   if ($(document).scrollTop() > $("#portfolio").position().top) {
     $("footer").css("visibility", "visible").fadeIn(500);
-    console.log($(document).scrollTop(), $("#portfolio").position().top);
   } else {
     $("footer").css("visibility", "hidden").fadeOut(500);
   };
@@ -54,24 +53,29 @@ $(window).on("scroll touchmove", function () {
 function backlight_over(n) {
   var text_block = document.getElementsByClassName("backlight_black_text");
   var subhead_block = document.getElementsByClassName("backlight_orange_text");
-  var block_shadow = document.get
+  var block_shadow = document.getElementsByClassName("resume-item");
   for (let i = (2*n-2); i <= (2*n-1); i++) {
     text_block[i].style.color = "black";
     text_block[i].style.transition = "all 1s";
   }
   subhead_block[n - 1].style.color = "rgba(80, 50, 200, 0.9)";
   subhead_block[n - 1].style.transition = "all 1s";
+  block_shadow[n - 1].style.boxShadow = "0px 0px 20px 20px rgba(100,100,100,0.5)";
+  block_shadow[n - 1].style.transition = "all 1s";
 }
 
 function backlight_out(n) {
   var text_block = document.getElementsByClassName("backlight_black_text");
   var subhead_block = document.getElementsByClassName("backlight_orange_text");
+  var block_shadow = document.getElementsByClassName("resume-item");
   for (let i = (2*n-2); i <= (2*n-1); i++) {
     text_block[i].style.color = "#868e96";
     text_block[i].style.transition = "all 1s";
   }
-  subhead_block[n-1].style.color = "#868e96";
-  subhead_block[n-1].style.transition = "all 1s";
+  subhead_block[n - 1].style.color = "#868e96";
+  subhead_block[n - 1].style.transition = "all 1s";
+  block_shadow[n - 1].style.boxShadow = "none";
+  block_shadow[n - 1].style.transition = "all 1s";
 }
 
 
@@ -99,4 +103,37 @@ function startRotate(event){
 function stopRotate(event){
   const cardItem = this.querySelector(".card-item");
   cardItem.style.transform = "rotate(0)";
+}
+
+
+
+
+
+
+
+
+var timeout;
+$('.scene').mousemove(function(e){
+  if(timeout) clearTimeout(timeout);
+  setTimeout(callParallax.bind(null, e), 200);
+
+});
+
+function callParallax(e){
+  parallaxIt(e, '.layer.layer_1', -100);
+  parallaxIt(e, '.layer.layer_2', -70);
+  parallaxIt(e, '.layer.layer_3', -50);
+  parallaxIt(e, '.scene', -30);
+}
+
+function parallaxIt(e, target, movement){
+  var $this = $('.scene');
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width()/2) / $this.width() * movement,
+    y: (relY - $this.height()/2) / $this.height() * movement,
+    ease: Power2.easeOut
+  })
 }
